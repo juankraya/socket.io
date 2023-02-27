03d5ad47dfbe0f72505aa31f6ebe6b2a9a5f5d55
juan carlos raya <Juank@iMac-de-juan.local>
2023-02-27 09:27:47 +0100

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Server } from 'socket.io'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on("connection", (socket) => {
+      socket.on('input-change', input => {
+        console.log(input, socket.id)
+        socket.broadcast.emit('update-input', {body: input})
+      })
+    })
+  }
+  res.end()
 }
+
+export default SocketHandler
